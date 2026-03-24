344ae9d120966b1e00220217c464981dc264f4c0
Sameer-Git-Repo <sameermahato360@gmail.com>
2026-03-24 15:01:02 +0530

    Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,20 +1,3 @@
-# def selectionsort(nums):
-#     n=len(nums)
-#     for i in range(n):
-#         min=nums[i]
-#         index=i
-#         for j in range(i+1,n):
-#             if(nums[j]<min):
-#                 min=nums[j]
-#                 index=j
-#         temp=nums[i]
-#         nums[i]=nums[index]
-#         nums[index]=temp
-#     print(lst)
-
-# lst=[5,1,1,2,0,0,15,22,11,50,23,67,34]
-# selectionsort(lst)   
-
 def selectionsort(nums):
     l = len(nums)
     for i in range(l):
