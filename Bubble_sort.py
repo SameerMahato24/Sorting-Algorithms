368cf63791f2ed048530888878054ef3c4253bdc
Sameer-Git-Repo <sameermahato360@gmail.com>
2026-03-24 14:58:45 +0530

    Bubble_sort.py
    Contains two methods(code) to solve the problems.

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,21 +1,24 @@
-# def bubblesort(lst):
-#     l=len(lst)
-#     for i in range(l):
-#         isSwap=False
-#         for j in range(l-i-1):
-#             if(lst[j]>lst[j+1]):
-#                 temp=lst[j]
-#                 lst[j]=lst[j+1]
-#                 lst[j+1]=temp
-#                 isSwap=True
+# Code 1
+def bubblesort(lst):
+    l=len(lst)
+    for i in range(l):
+        isSwap=False
+        for j in range(l-i-1):
+            if(lst[j]>lst[j+1]):
+                temp=lst[j]
+                lst[j]=lst[j+1]
+                lst[j+1]=temp
+                isSwap=True
 
-#         if not isSwap:
-#             break        
+        if not isSwap:
+            break        
 
-#     print(lst)
-# lst=[115,13,56,23,44,87,91,42]
-# bubblesort(lst)            
+    print(lst)
+lst=[115,13,56,23,44,87,91,42]
+bubblesort(lst)            
 
+
+# Code 2
 def bubblesort(lst):
     l = len(lst)
     for i in range(l-2,-1,-1):
